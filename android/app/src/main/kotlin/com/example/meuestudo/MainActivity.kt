5799b30fbae1c31722c236350f84ac1fed987671
Your Name <you@example.com>
2022-08-30 09:21:19 -0300

    initial revision

---
+++ android/app/src/main/kotlin/com/example/meuestudo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meuestudo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/pages/home_page.dart
@@ -0,0 +1,107 @@
+
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(title: Text("Meu Estudo"),
+      actions: [Icon(Icons.search)],
+      centerTitle: true,),
+      bottomNavigationBar: 
+      BottomNavigationBar (
+        items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home),label: "inicio"),
+        BottomNavigationBarItem(icon: Icon(Icons.timelapse),label: "Horarios"),
+        BottomNavigationBarItem(icon: Icon(Icons.settings),label: "Configuracao"),
+        
+      ]),
+      drawer: Drawer(
+        child: Column(children: [
+          UserAccountsDrawerHeader(accountName: Text("Cleiton Jonnas"), accountEmail: Text("cleitinhoextreme007@gmail.com",),
+          currentAccountPicture: ClipOval(child:  Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTggauC9GzZ20cDt94Nzm2-z-qvmi9Jrh1JK-6n1HG-derBaewvGMJTsHMcw8ndDX7Fjqc&usqp=CAU"),),
+          ),
+          ListTile(
+            title: Text("Conta"),
+            leading: Icon(Icons.person),
+          ),
+
+           ListTile(
+            title: Text("notificacoes"),
+            leading: Icon(Icons.push_pin),
+          ),
+
+           ListTile(
+            title: Text("Atividades"),
+            leading: Icon(Icons.list_alt),
+          ),
+
+           ListTile(
+            title: Text("Atividades Pendentes"),
+            leading: Icon(Icons.list),
+          ),
+
+           ListTile(
+            title: Text("Materias arquivadas"),
+            leading: Icon(Icons.person),
+          ),
+
+           ListTile(
+            title: Text("Sair"),
+            leading: Icon(Icons.logout),
+          ),
+
+        ],),
+      ),
+
+      body: GridView(
+        padding: EdgeInsets.fromLTRB(18, 14, 18, 14),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 14, crossAxisSpacing: 14),
+        
+        children: [
+        Card(
+          shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          color: Color.fromARGB(255, 79, 8, 3),
+          child: Container(
+            //color: Colors.blue,
+          ),
+        ),
+
+        Card(
+          shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          color: Colors.red,
+          child: Container(
+            //color: Colors.blue,
+          ),
+        ),
+       Card(
+          shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          color: Colors.red,
+          child: Container(
+            //color: Colors.blue,
+          ),
+        ),
+        Card(
+          shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          color: Colors.red,
+          child: Container(
+            //color: Colors.blue,
+          ),
+        ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (() {
+        
+      }),
+      child: Icon(Icons.add),
+      
+    )
+    );
+    
+  }
+}
